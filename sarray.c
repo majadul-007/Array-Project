11fa734478c52948b68ebe0d212ba97136bc7b75
tusarhaque <tusarhaque12@gmail.com>
2018-11-13 19:48:25 +0600

    demo

---
+++ sarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int i[5];
+    int j, k,  l=0;
+    for(j=0; j<5; j++){
+        i[j] = j;
+        
+    }
+    for(k=0; k<5; k++){
+        l += i[j]
+    ;
+    // return 0;
+}
+printf("Arrays are: %d ", l);
+return 0;
+}
